5f2d90df1f2c0192e01bbd6c46b65c392e88a10b
biones <iijimasat@gmail.com>
2021-05-13 15:05:45 +0900

    Update main.js

--- main.js
+++ main.js
@@ -11,14 +11,7 @@ Vue.config.productionTip = false
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAuj1NTt9MyhGRv393WhQGqwHwD5HVVb6w",
-  authDomain: "kakeibo9.firebaseapp.com",
-  databaseURL: "https://kakeibo9.firebaseio.com",
-  projectId: "kakeibo9",
-  storageBucket: "kakeibo9.appspot.com",
-  messagingSenderId: "175219230955",
-  appId: "1:175219230955:web:fd8b8494d2b94ce5eb56ae",
-  measurementId: "G-V0SNC3MWV0"
+
 };
 import firebase from 'firebase'
 import 'firebase/firestore'
